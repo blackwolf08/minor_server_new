8742c80809a2fae50a094e767466125414cdc8aa
Sunny Dhama <31814079+blackwolf08@users.noreply.github.com>
2019-11-25 22:02:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ import tensorflow
 import keras
 from keras.preprocessing import image
 from keras.models import load_model
-from flask_ngrok import run_with_ngrok
 from PIL import ImageTk, Image
 import fpdf
 from datetime import date
@@ -14,9 +13,5 @@ app = Flask(__name__)
 
 
 @app.route('/', methods = ['GET'])
-def get_root():
+def hello():
     return "Hello World"
-
-if __name__ == '__main__':
-    app.config["JSON_SORT_KEYS"] = False
-    app.run(host='127.0.0.1', port=8080, debug=True)
